64453c6803fe5bbf87dada0b019f4a879c912766
Geethanjali0225 <geetanjali.c2k@gmail.com>
2023-10-26 16:44:58 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+import time
+import psutil
+
+while True:
+    print("Hello, World!")
+    cpu_percent = psutil.cpu_percent(interval=1)
+    mem_info = psutil.virtual_memory()
+    print(f"CPU Usage: {cpu_percent}%")
+    print(f"Memory Usage: {mem_info.percent}%")
+    time.sleep(10)
